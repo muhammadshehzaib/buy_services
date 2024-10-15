74a368cea84a0b7c8ce25bebf9df3ef258dbf1dc
ZaheerAfzal1408 <muhammadzaheera1408@gmail.com>
2024-10-15 09:36:26 -0700

    review

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import HeroSection from "@/components/HeroSection";
 import Header from "../components/Header"
 import Slider from "@/components/Slider";
 import Card from "@/components/Card";
+import Reviews from "@/components/Reviews";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <Slider />
         < HeroSection/>
         < Card/>
+        < Reviews/>
     </div>
   );
 }
